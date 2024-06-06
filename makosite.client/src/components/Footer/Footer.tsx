6cd5a2e1b8a1b45246a36ee28dbeeb50bfd1aa93
grinyuk <grinyuk309@gmail.com>
2024-06-06 16:05:36 +0300

    added font-awesome and styles for footer

--- makosite.client/src/components/Footer/Footer.tsx
+++ makosite.client/src/components/Footer/Footer.tsx
@@ -2,6 +2,8 @@ import React, {FC} from 'react';
 import {Logo} from "../Logo/Logo.tsx";
 import css from './Footer.module.css';
 import {Link} from "react-router-dom";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import { faDiscord, faFigma, faGithub } from '@fortawesome/free-brands-svg-icons'
 
 const Footer : FC = () => {
     return (
@@ -16,11 +18,16 @@ const Footer : FC = () => {
                     <li><Link to={'/privacy-policy'}>Privacy policy</Link></li>
                 </ul>
 
-                <div>
-                    <a href="#">figma</a>
-                    <a href="#">discord</a>
-                    <a href="#">github</a>
+                <div className={css.footerMedia}>
+                    <a href="https://www.figma.com/design/oddbpepKagAU9WOg11Bqwx/Makosite?node-id=0-1&t=OTTVh6DygczKhv51-1"><FontAwesomeIcon size={'2x'} icon={faFigma} /></a>
+                    <a href="https://discord.com/"><FontAwesomeIcon size={'2x'} icon={faDiscord} /></a>
+                    <a href="https://github.com/MakoginDenys/Makosite"><FontAwesomeIcon size={'2x'} icon={faGithub} /></a>
                 </div>
+            </div>
+
+            <div className={[css.flexContainer, css.footerAuthor].join(' ')}>
+                <span>© 2024 Ukrainian donation platform</span>
+                <span>Developed by Vasyl Hryniuk and Denys Makogin</span>
             </div>
         </div>
     );
