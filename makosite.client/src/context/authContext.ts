4d2a8e3c4b0a3791e384627c716f0234ef87e67f
grinyuk <grinyuk309@gmail.com>
2024-06-06 18:45:26 +0300

    migrated from Context API to Redux/toolkit

--- makosite.client/src/context/authContext.ts
+++
@@ -1,4 +0,0 @@
-import {IAuthContextProps} from "../interfaces/IAuthContextProps.ts";
-import {createContext} from "react";
-
-export const AuthContext = createContext<IAuthContextProps | undefined>(undefined);
