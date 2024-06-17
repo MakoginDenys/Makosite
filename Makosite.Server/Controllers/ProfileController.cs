d07f36f672d3eef8ea90239ef95ce5622b2b7f7d
Денис Макогін <denreadmi4@gmail.com>
2024-06-18 02:16:06 +0300

    added validation for existed users

--- Makosite.Server/Controllers/ProfileController.cs
+++ Makosite.Server/Controllers/ProfileController.cs
@@ -4,6 +4,7 @@ using Makosite.Server.Services.Interfaces;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Authorization;
+using Makosite.Server.Models;
 
 namespace Makosite.Server.Controllers
 {
@@ -17,10 +18,10 @@ namespace Makosite.Server.Controllers
             _userService = userService;
         }
         [HttpPost("update")]
-        public async Task<User> Update(string email, User newUser)
+        public async Task<AuthResponseModel> Update(string email, User newUser)
         {
             var user = await _userService.UpdateUserInformation(email, newUser);
-            if (user == null) return new User();
+            if (user == null) return new AuthResponseModel() { Success = false, Message = "Cannot find user" };
             return user;
         }
         [HttpGet("{username}")]
